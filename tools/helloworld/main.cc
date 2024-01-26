595fa9536d37c0f8675a733f4d1049d209cff78b
Kevin Dai <k22dai@uwaterloo.ca>
2024-01-26 13:53:15 -0500

    Initial commit 🎉
    Co-authored-by: Owen Zhu <tx2zhu@uwaterloo.ca>
    Co-authored-by: Larry <Kiwiegg@users.noreply.github.com>

---
+++ tools/helloworld/main.cc
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+extern "C" {
+#include "lexer.h"
+#include "parser.h"
+}
+
+int main(void) {
+    while (1) {
+        fflush(stdin);
+        char expr[256];
+
+        if (!fgets(expr, sizeof(expr), stdin)) {
+            continue;
+        }
+
+        YY_BUFFER_STATE state;
+
+        if (!(state = yy_scan_bytes(expr, strcspn(expr, "\n")))) {
+            continue;
+        }
+
+        int ret;
+
+        if (yyparse(&ret) == 0) {
+            printf("= %d\n", ret);
+        }
+
+        yy_delete_buffer(state);
+    }
+
+    return 0;
+}
