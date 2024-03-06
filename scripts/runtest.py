8e7a7355533352aef829f20885701801c9de5239
Kevin Dai <k22dai@uwaterloo.ca>
2024-03-06 16:09:50 -0500

    runtest.py: Sort the files alphabetically for better readability

--- scripts/runtest.py
+++ scripts/runtest.py
@@ -10,24 +10,24 @@ sys.path.append(os.path.join(os.path.dirname(__file__), "common.py"))
 from common import *
 
 num_crashes = 0
-ind = '⢎⡰⢎⡡⢎⡑⢎⠱⠎⡱⢊⡱⢌⡱⢆⡱'
+ind = "⢎⡰⢎⡡⢎⡑⢎⠱⠎⡱⢊⡱⢌⡱⢆⡱"
+
 
 # Run the tests
-def run_test(test, exp_rc):
+def run_test(test: str, exp_rc: int) -> int:
     global num_crashes
     global ind
-    print(f'[{ind[0]}{ind[1]}] Running {test:65.65}', end='\r', flush=True)
-    ind = ind[2:] + ind[:2] # Rotate the indicator
+    print(f"[{ind[0]}{ind[1]}] Running {test:65.65}", end="\r", flush=True)
+    ind = ind[2:] + ind[:2]  # Rotate the indicator
     test_path = os.path.join(test_dir, test)
     cmd = get_joosc_command(args.args, test_path, stdlib_dir)
     ret, _, _ = run_test_case(cmd)
-    print(' ' * 80, end='\r', flush=True)
+    print(" " * 80, end="\r", flush=True)
     if ret != 0 and ret != 42:
         num_crashes += 1
     if ret != exp_rc:
         print(f"{test} failed with return code {ret}, expected {exp_rc}")
-        if ret == 0 or ret == 42:
-            return 1
+        return 1
     return 0
 
 
@@ -51,25 +51,22 @@ if args.l:
 invalid_files = [x for x in test_names if x.startswith("Je_")]
 valid_files = [x for x in test_names if x not in invalid_files]
 
-# Count the failures by running the tests
-valid_failures = reduce(
-    lambda x, y: x + y,
-    [run_test(test, 0) for test in valid_files],
-)
-invalid_failures = reduce(
-    lambda x, y: x + y,
-    [run_test(test, 42) for test in invalid_files]
-)
-failures = valid_failures + invalid_failures + num_crashes
+# Sort the files alphabetically
+valid_files.sort()
+invalid_files.sort()
 
-print('---')
+# Calculate the number of failures
+valid_failures = sum([run_test(test, 0) for test in valid_files])
+print("---")
+invalid_failures = sum([run_test(test, 42) for test in invalid_files])
+print("---")
+failures = valid_failures + invalid_failures
+
+# Print the summary report
 print(
-    f"Total failures: {failures}/{len(test_names)} or {len(test_names) - failures}/{len(test_names)} passing"
+    f"Total failures: {failures}/{len(test_names)}"
+    f" or {len(test_names) - failures}/{len(test_names)} passing"
 )
-print(
-    f"Incorrectly rejected {valid_failures}/{len(valid_files)} valid tests"
-)
-print(
-    f"Incorrectly accepted {invalid_failures}/{len(invalid_files)} invalid tests"
-)
+print(f"Incorrectly rejected {valid_failures}/{len(valid_files)} valid tests")
+print(f"Incorrectly accepted {invalid_failures}/{len(invalid_files)} invalid tests")
 print(f"Crashed on {num_crashes}/{len(test_names)} tests")
