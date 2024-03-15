4e6fdcb6cf8e785ff079fc3017a2d029dbf137e1
Owen Zhu <tx2zhu@uwaterloo.ca>
2024-03-15 17:20:26 -0400

    Merge conflict

--- tests/data/basic_test_owen.java
+++ tests/data/basic_test_owen.java
@@ -1,18 +1,18 @@
 public class basic_test_owen {
     public basic_test_owen() {}
 
-    /*public int test1() {
-        while (true) { }
+    public int test1() {
+        for(;true;) {}
 	      return 123;
-    }*/
+    }
 
     public int test(int j) {
         int x=123;
-      if (x==122) while (true) {}
+      if (x==122) for(;;) {}
       return x;
     }
 
     public int method() {
-      while (false || true) {}
+      for(;;) {}
     }
 }
